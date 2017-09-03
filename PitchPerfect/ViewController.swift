ce6f1c2cf2743d0f7edd5d7d988b93913ffb0cc0
Unknown <mlgrier@gmail.com>
2017-09-03 08:28:09 -0500

    added stop button and label

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordTapped(_ sender: Any) {
+        
+        recordingLabel.text = "Recording in Progress"
+    }
 
+    @IBAction func stopTapped(_ sender: Any) {
+        
+        recordingLabel.text = "Recording Stopped"
+    }
 }
 
