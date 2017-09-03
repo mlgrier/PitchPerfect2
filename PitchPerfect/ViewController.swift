e67f86c705d0e82c78330b45b95ae0ae67bfead8
Unknown <mlgrier@gmail.com>
2017-09-03 08:58:55 -0500

    added the buttom images and fix sizes

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -12,9 +12,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
     
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
     }
 
     override func didReceiveMemoryWarning() {
